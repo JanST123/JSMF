b47bba18354c05bdbe59e6f6941934e260abd3f4
Jan <jstuhlmann@gmail.com>
2019-02-01 11:55:11 +0100

    fix autoloading

--- src/Application.php
+++ src/Application.php
@@ -21,6 +21,131 @@ class Application {
   private static $_hooks=[];
 
 
+  public static function registerAutoloader() {
+
+
+    spl_autoload_register(function ($class) use (&$cache) {
+      $moduleDir = Config::get('modulePath');
+      $namespace = Config::get('applicationNamespace');
+      if (strpos($class, $namespace) !== 0) {
+        return; // only used for our application namespace
+      }
+
+      // if module dir ends with class start replace that
+      $clsSplit = explode('\\', $class);
+      while (count($clsSplit)) {
+        $test=implode(DIRECTORY_SEPARATOR, $clsSplit);
+        if (strpos($moduleDir, $test) == strlen($moduleDir) - strlen($test)) {
+          $moduleDir = substr($moduleDir, 0, strpos($moduleDir, $test) - 1);
+        }
+        array_pop($clsSplit);
+      }
+
+
+      $path = ($moduleDir . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
+
+      self::logg('======================');
+      self::logg('Loading class ' . $class . ' searching in ' . $path);
+      if (isset($cache[$path])) {
+        $path = $cache[$path];
+        self::logg('Found path in cache, using ' . $path);
+      }
+
+
+      if (!file_exists($path)) {
+        $pathOriginal = $path;
+
+        self::logg('not found in path, trying case insensitive searching');
+
+        // try to get it case insensitive
+        // upwalk the path until it exists
+        $pathExpl = explode(DIRECTORY_SEPARATOR, str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
+        $classExpl = explode('\\', $class);
+        $classNotExists = [];
+        do {
+          array_pop($pathExpl);
+          array_unshift($classNotExists, array_pop($classExpl));
+        } while (count($pathExpl) && !file_exists(BASE_PATH . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR,
+            $pathExpl)));
+
+        $lastExistingPath = BASE_PATH . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR,
+            $pathExpl) . DIRECTORY_SEPARATOR;
+        self::logg('path exists up to: ' . $lastExistingPath);
+
+        // now we retrieve the lowercased directory structure from the last existing directory and try to match it case insensitive
+        do {
+          self::logg('getting directory ' . $lastExistingPath);
+          $structure = self::getLowercasedDirectory($lastExistingPath);
+          $checkLcPath = strtolower($lastExistingPath . $classNotExists[0] . (count($classNotExists) == 1 ? '.php' : ''));
+          self::logg('Check lc path: ' . $checkLcPath);
+          if (isset($structure[$checkLcPath])) {
+            // the dir/file exists with another case, check if we can resolve the path now
+            self::logg('lowcased path ' . $structure[$checkLcPath] . ' does exist!');
+            $lastExistingPath = $structure[$checkLcPath] . DIRECTORY_SEPARATOR;
+
+
+            if (count($classNotExists) == 1) {
+              $path = $structure[$checkLcPath];
+            } else {
+              array_shift($classNotExists);
+              $path = $lastExistingPath . implode(DIRECTORY_SEPARATOR, $classNotExists) . '.php';
+            }
+            self::logg('Checking ' . $path);
+          } else {
+            self::logg('even case-insensitive the path cannot be resolved... ' . $lastExistingPath . strtolower($classNotExists[0]) . ') aborting');
+            break;
+          }
+        } while (!file_exists($path) && count($classNotExists));
+
+        if (file_exists($path)) {
+          $cache[$pathOriginal] = $path;
+
+          $cacheContent='<?php' . "\n" . '$cache=[';
+          foreach ($cache as $k => $v) {
+            $cacheContent.='\'' . $k . '\'=>\'' . str_replace('\'', '\\\'', $v) . '\',';
+          }
+          $cacheContent.='];';
+
+          file_put_contents(CACHE_FILE, $cacheContent);
+        }
+      }
+
+      if (file_exists($path)) {
+        self::logg('found in ' . $path);
+        include($path);
+      } else {
+        self::logg('class not found');
+      }
+    }, true, true);
+
+
+
+  }
+
+  /**
+   * get directory content (non-recursive) as array. Keys contain the low-cased path
+   * @param String $dir
+   * @return Array
+   **/
+  private static function getLowercasedDirectory($dir) {
+    $structure = Array();
+    $dh = opendir($dir);
+    while ($file = readdir($dh)) {
+      if ($file == '.' || $file == '..' || strpos($file, '.') === 0 || !is_readable($dir . $file)) {
+        continue;
+      }
+
+      $structure[strtolower($dir . $file)] = $dir . $file;
+    }
+    closedir($dh);
+
+    return $structure;
+  }
+
+  private static function logg($text) {
+    //echo "\n<br />" . $text;
+  }
+
   /**
    * inits and runs the application
    * @param String|index $defaultModule
@@ -47,7 +172,7 @@ class Application {
       Template::setTemplateDir($moduleDir . DIRECTORY_SEPARATOR . ucfirst(Request::getModule($defaultModule)) . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . ucfirst(Request::getController($defaultController)) . DIRECTORY_SEPARATOR);
 
       // instanciate the right controller
-      self::$_controller=Controller::factory('\\' . $namespace . '\\modules', Request::getModule($defaultModule), Request::getController($defaultController));
+      self::$_controller=Controller::factory('\\' . $namespace . '\\Modules', Request::getModule($defaultModule), Request::getController($defaultController));
 
       // set the action (controller will instanciate the template then)
       self::$_controller->setAction(Request::getAction($defaultAction));
